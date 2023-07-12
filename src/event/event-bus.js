fb09e77a1b42d784486b6b5548b74d26cd926aa1
Tomáš Glawaty <tomasglawaty@icloud.com>
2023-07-12 04:53:14 +0200

    Sorted event listeners ( - added the optional argument `priority` for the method `EventBus.subscribe()` - event `amp:banner:state-changed` that is subscriber by `BannerInteractionWatcher` has priority -100 (will be invoked as the last)

--- src/event/event-bus.js
+++ src/event/event-bus.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const SequenceGenerator = require('../utils/sequence-generator');
 const internal = require('../utils/internal-state')();
 
@@ -6,9 +5,10 @@ class EventBus {
     constructor() {
         internal(this).generator = new SequenceGenerator();
         internal(this).listeners = {};
+        internal(this).sortedListeners = {};
     }
 
-    subscribe(event, callback, scope = null) {
+    subscribe(event, callback, scope = null, priority = 0) {
         if (typeof callback !== 'function') {
             throw new TypeError('Listener must be of type function.');
         }
@@ -20,8 +20,13 @@ class EventBus {
         _internal.listeners[event] = _internal.listeners[event] || {};
         _internal.listeners[event][id] = {
             callback: callback,
-            scope: scope
+            scope: scope,
+            priority: priority,
         };
+
+        if (event in _internal.sortedListeners) {
+            delete _internal.sortedListeners[event];
+        }
 
         return function () {
             if (event in _internal.listeners && id in _internal.listeners[event]) {
@@ -31,15 +36,25 @@ class EventBus {
     }
 
     dispatch(event, ...args) {
-        const listeners = internal(this).listeners[event] || {};
-
-        _.forEach(listeners, listener => {
+        for (let listener of this.getSortedListeners(event)) {
             listener.callback.call(listener.scope, ...args);
-        });
+        }
     }
 
     reset() {
         internal(this).listeners = {};
+    }
+
+    getSortedListeners(event) {
+        if (event in internal(this).sortedListeners) {
+            return internal(this).sortedListeners[event];
+        }
+
+        const listeners = Object.values(internal(this).listeners[event] || {}).sort((left, right) => {
+            return left.priority > right.priority ? -1 : (left.priority < right.priority ? 1 : 0);
+        });
+
+        return internal(this).sortedListeners[event] = listeners;
     }
 }
 
